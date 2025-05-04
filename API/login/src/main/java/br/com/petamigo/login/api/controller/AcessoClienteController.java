c86f8759b37085449025e3fab2d92eafaff125be
Mauro Degaspari <mauro_degaspari@hotmail.com>
2025-05-04 10:33:20 -0300

    teste com banco de dados funcionarios.

--- API/login/src/main/java/br/com/petamigo/login/api/controller/AcessoClienteController.java
+++ API/login/src/main/java/br/com/petamigo/login/api/controller/AcessoClienteController.java
@@ -10,22 +10,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.petamigo.login.domain.service.AcessoClienteService;
+import br.com.petamigo.login.domain.service.AcessoFuncionarioService;
 import br.com.petamigo.login.infrastruture.model.AcessoClienteModel;
+import br.com.petamigo.login.infrastruture.model.AcessoFuncionarioModel;
 
 @Controller
 @RequestMapping("/loginPetAmigo")
 public class AcessoClienteController {
 	
 	@Autowired
-	private AcessoClienteService service;
+	private AcessoClienteService serviceClientes;
+	@Autowired
+	private AcessoFuncionarioService serviceFuncionario;
 	
-	@GetMapping(value = "/listarTodos")
+	@GetMapping(value = "/listarTodosClientes")
 	public ResponseEntity<List<AcessoClienteModel>> testeCLientes(){
 		
-		List<AcessoClienteModel> clientes = service.testeListendoCLienteService();
+		List<AcessoClienteModel> clientes = serviceClientes.testeListendoCLienteService();
 
 		return clientes.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
 				: new ResponseEntity<List<AcessoClienteModel>>(clientes, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/listarTodosFuncionarios")
+	public ResponseEntity<List<AcessoFuncionarioModel>> testeFuincionario(){
+		
+		List<AcessoFuncionarioModel> funcionarios = serviceFuncionario.testeListendoFuncionariosService();
+
+		return funcionarios.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
+				: new ResponseEntity<List<AcessoFuncionarioModel>>(funcionarios, HttpStatus.OK);
+	}
+	
+	@GetMapping(value ="/LoginFuncionario/{email}")
+	public ResponseEntity<AcessoClienteModel> loginCliente(String usuario, String senha) {
+		return null;
 	}
 	
 
